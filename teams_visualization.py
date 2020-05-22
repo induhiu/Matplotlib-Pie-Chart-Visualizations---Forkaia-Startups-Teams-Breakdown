58f1bf3c90ef60af21f76cf0dd6344c0a01921d3
induhiu <induhiu@hamilton.edu>
2020-05-21 22:21:33 -0400

    Updated legend location

--- teams_visualization.py
+++ teams_visualization.py
@@ -17,7 +17,9 @@ data_dictionary = pd.read_excel("DATA DICTIONARY.xlsx")
 member_and_teams = pd.read_excel("TEAM VISUALIZATION.xlsx")
 
 def main():
-    ''' '''
+    ''' The main function that serves as entry point. Calls the functions that 
+        extract the data, find count of people per team per startup, 
+        and visualizes the results '''
     # Get a list of all unique startups and a dictionary that
     # associates each team with all of its respective subteams
     startups, teams_and_subteams = extract_data()
@@ -32,8 +34,8 @@ def main():
 
 
 def extract_data():
-    ''' Extracts and cleans startups data while also building a navigator
-        used to relate subteams with their appropriate team '''
+    ''' Extracts and cleans startups data while also building a dictionary
+        used to associate subteams with their appropriate team '''
     # First: Get all unique startups
     # Getting values from STARTUP column, splitting them into lists
     # and flattening the list of lists into a unique list
@@ -68,6 +70,7 @@ def extract_data():
         
     return (startups, teams_and_subteams)
 
+
 def team_count(startups, teams_and_subteams):
     ''' Updates the teams' member counts within each startup by iterating
         member by member and checking which startups they are part of, and
@@ -93,6 +96,47 @@ def team_count(startups, teams_and_subteams):
         
     return startups
 
+
+def visualize(startups_team_counts):
+    ''' Creates and saves a pie chart for each startup showing the
+        number of members for each team '''
+    teams = ["BUSINESS TEAM", "CREATIVE TEAM", "DATA TEAM", 
+              "DEVELOPERS TEAM", "PM"]  # to be used as labels
+    curr_dir = "C:\\Users\\nderi\\FORKAIA\\Project 1 - Data Visualization" +\
+                 "\\Startup Visualizations\\"
+    for startup in startups_team_counts:
+        if startup != "DATA SCIENCE ARMY":
+            # Numbers for each team
+            number_per_team = [startups_team_counts[startup][team] 
+                                for team in teams]
+
+            # Create pie chart with team counts as sizes
+            _, ax1 = plt.subplots()
+            wedges, _, autotexts = ax1.pie(number_per_team, 
+                autopct=lambda pct: func(pct, number_per_team), shadow=True)
+            ax1.legend(wedges, [team.split()[0] for team in teams], title="Teams", 
+                    loc="lower right", bbox_to_anchor=(0.62, -0.167, 0.5, 1))
+            plt.setp(autotexts, size=8, weight="bold")
+            ax1.axis('equal')
+            ax1.set_title(startup)
+            plt.savefig(curr_dir + startup + '.png')
+            plt.close()
+        else:
+            # Exception for Data Science Army - one big team
+            _, ax1 = plt.subplots()
+            army_size = [startups_team_counts[startup]]
+            ax1.pie(army_size, autopct=lambda pct: func(pct, number_per_team), shadow=True)
+            ax1.axis('equal')
+            ax1.set_title(startup)
+            plt.savefig(curr_dir + startup + '.png')
+            plt.close()
+
+
+def func(pct, allvals):
+    ''' Returns a formatted string that allows pie chart to display both
+        percentages and actual count of members in each team '''
+    absolute = int(pct/100.*np.sum(allvals))
+    return "{:.1f}%\n({:d} people)".format(pct, absolute)
 
 if __name__ == "__main__":
     main()
